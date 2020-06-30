4c3ae649d472a0757b0e01438b897073257c0d5a
jy <977996067@qq.com>
2020-06-30 21:25:29 +0800

    :sparkles: Introduce embedded-postgresql

--- src/test/java/com/example/service/UserServiceTest.java
+++ src/test/java/com/example/service/UserServiceTest.java
@@ -7,13 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
@@ -31,19 +27,8 @@ import static org.springframework.test.context.jdbc.SqlMergeMode.MergeMode.MERGE
 @SqlMergeMode(MERGE)
 class UserServiceTest {
 
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:alpine")
-        .withUsername("jy")
-        .withPassword("123123")
-        .withDatabaseName("demo");
-
     @Autowired
     private UserService userService;
-
-    @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    }
 
     @Test
     @Transactional
